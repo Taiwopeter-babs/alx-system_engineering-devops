a93b2bd51b0b8440d1286043dc2678bea1f12c5b
Taiwo Babalola <babalolataiwop@gmail.com>
2023-01-31 21:59:24 +0100

    Find specified values

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(\[from:([a-zA-Z]+|\+?\w+)).+(\[to:)([a-zA-Z]+|\+?\w+).+(\[flags):(.\w+.+-1)\]/).join(",")
+puts ARGV[0].scan(/\[from:([a-zA-Z]+|\+?\d+)?\]\s(\[to:)([a-zA-Z]+|\+?\d+)?\]\s(\[flags):(.\w+.+-1)?\]/).join(",")
