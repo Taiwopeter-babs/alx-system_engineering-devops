228ef4f6af74828f728446637d5fa23f15d4fb1f
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-24 13:21:18 +0100

    [fix] with puppet lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,9 +3,9 @@ $str = 'I love Puppet'
 
 file { '/tmp/school':
   ensure  => 'present',
-  owner   => "www-data",
-  group   => "www-data",
-  mode => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
   content => $str,
 }
 
