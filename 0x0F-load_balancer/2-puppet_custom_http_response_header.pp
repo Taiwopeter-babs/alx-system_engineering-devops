1ea0ec86c58466304ef12c20ae6cbef771b7218b
Taiwo Babalola <babalolataiwop@gmail.com>
2023-04-04 13:36:05 +0100

    configure nginx server with puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,8 +3,7 @@
 exec { 'configure a nginx server':
   command  => 'sudo apt-get -y update;
 	sudo apt-get -y install nginx;
-	sed -i "/server { listen 80 default_server; listen [::]:80 default_server;
-	root /var/www/html; add_header X-Served-By $hostname;}" /etc/nginx/sites-available/default;
+	sed -i "/index.nginx-debian.html/a add_header X-Served-By $hostname;" /etc/nginx/sites-available/default;
 	sudo service nginx restart;',
   provider => 'shell',
 }
