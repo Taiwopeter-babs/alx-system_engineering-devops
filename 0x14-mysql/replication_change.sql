ffaf9a0156959613bf34c58e40a2e119cec0b55f
Taiwo Babalola <babalolataiwop@gmail.com>
2023-04-18 19:41:32 +0100

    change syntax for mysql version 5.7*

--- 0x14-mysql/replication_change.sql
+++ 0x14-mysql/replication_change.sql
@@ -1,7 +1,7 @@
 -- configure replication on replica server
-CHANGE REPLICATION SOURCE TO
-SOURCE_HOST='18.235.234.96',
-SOURCE_USER='replica_user',
-SOURCE_PASSWORD='projectcorrection280hbtn',
-SOURCE_LOG_FILE='mysql-bin.000001',
-SOURCE_LOG_POS=154;
+CHANGE master TO
+master_host='18.235.234.96',
+master_user='replica_user',
+master_password='projectcorrection280hbtn',
+master_log_file='mysql-bin.000001',
+master_log_pos=154;
