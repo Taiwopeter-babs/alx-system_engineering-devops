788752ef8cbc54b0dd1920f35f414634916cd703
Taiwo Babalola <babalolataiwop@gmail.com>
2023-04-24 22:02:02 +0100

    test script

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,29 +10,25 @@ def gather_data(emp_id):
     """returns data about and employee from an api"""
 
     url_todo = "https://jsonplaceholder.typicode.com/todos"
-    url_users = "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
+    url_users = "https://jsonplaceholder.typicode.com/users?id={}"
 
-    try:
-        payload = {"userId": emp_id}
+    payload = {"userId": emp_id}
 
-        # make requests via the APIs
-        todo = requests.get(url_todo, params=payload).json()
-        resp_user = requests.get(url_users).json()
+    # make requests via the APIs
+    todo = requests.get(url_todo, params=payload).json()
+    resp_user = requests.get(url_users.format(emp_id)).json()[0]
 
-        # get the total number of tasks for employee and the employee's name
-        emp_name = resp_user.get("name")
+    # get the total number of tasks for employee and the employee's name
+    emp_name = resp_user.get("name")
 
-        # get the completed tasks
-        done = [task.get("title") for task in todo if task.get("completed")]
-        # output
-        first_line = "Employee {} is done with tasks({}/{}):"
-        print(first_line.format(emp_name, len(done), len(todo)))
+    # get the completed tasks
+    done = [task.get("title") for task in todo if task.get("completed")]
+    # output
+    first_line = "Employee {} is done with tasks({}/{}):"
+    print(first_line.format(emp_name, len(done), len(todo)))
 
-        # print the title of the completed tasks
-        [print("\t {}".format(task)) for task in done]
-
-    except requests.exceptions.RequestException:
-        return
+    # print the title of the completed tasks
+    [print("\t {}".format(task)) for task in done]
 
 
 if __name__ == "__main__":
