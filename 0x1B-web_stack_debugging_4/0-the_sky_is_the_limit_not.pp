6eb4fe4feb6139903d416098bac47165188ce432
Taiwo Babalola <babalolataiwop@gmail.com>
2023-05-18 12:11:10 +0100

    add fixes for nginx server

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# This script increses the number of open file descriptors
+# the web server open per request
+
+exec {'nginx-server-fix':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  onlyif  => 'test -e /etc/default/nginx',
+}
+
+exec {'restart nginx':
+  command => 'sudo service nginx restart',
+  path    => '/bin/',
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,1 @@
+#    Debugging web stack exercise 4
