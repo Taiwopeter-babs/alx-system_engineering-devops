69450df6c98fb84d835dd0728731cc34f34c698d
Taiwo Babalola <babalolataiwop@gmail.com>
2023-05-18 12:44:19 +0100

    add fixes for nginx server

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # This script increses the number of open file descriptors
 # the web server open per request
 
-exec {'sed -i "s/15/4096/" /etc/default/nginx':
-  path   => '/usr/bin:/usr/sbin:/bin',
-  onlyif => 'test -e /etc/default/nginx',
+exec {'fix-nginx-server-too-many-open-files-error':
+command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path  => '/usr/bin:/usr/sbin:/bin',
 }
 
 exec {'restart nginx':
