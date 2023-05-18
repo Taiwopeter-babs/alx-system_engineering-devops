5b3acd39cfb21aa1e3c9c58785b0afe416125bcf
Taiwo Babalola <babalolataiwop@gmail.com>
2023-05-18 12:40:17 +0100

    add fixes for nginx server

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,11 +2,11 @@
 # the web server open per request
 
 exec {'sed -i "s/15/4096/" /etc/default/nginx':
-  path => '/usr/bin:/usr/sbin:/bin',
-  onlyif  => 'test -e /etc/default/nginx',
+  path   => '/usr/bin:/usr/sbin:/bin',
+  onlyif => 'test -e /etc/default/nginx',
 }
 
 exec {'restart nginx':
   command => 'sudo service nginx restart',
-  path    => '/usr/bin/',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
