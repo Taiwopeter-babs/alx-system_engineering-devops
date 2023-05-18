65ab4a526af64b48f53332c11d954785996f44f7
Taiwo Babalola <babalolataiwop@gmail.com>
2023-05-18 14:23:36 +0100

    fix user limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,9 +3,11 @@
 $com = 'sed -i'
 $filename = '/etc/security/limits.conf'
 $const = 'holberton'
+$com_one = "${com} 's/${const} hard.*/${const} hard nofile 20000/' ${filename};"
+$com_two = "${com} 's/${const} soft.*/${const} soft nofile 20000/' ${filename}"
+
 exec {'fix-user-limit':
-command  => "${com} 's/${const} hard.*/${const} hard nofile 20000/' ${filename}; " +
-      "${com} 's/${const} soft.*/${const} soft nofile 20000/' ${filename}",
+command  => "${com_one}${com_two}",
   path   => '/usr/bin:/usr/sbin:/bin',
   onlyif => 'test -e /etc/security/limits.conf',
 }
