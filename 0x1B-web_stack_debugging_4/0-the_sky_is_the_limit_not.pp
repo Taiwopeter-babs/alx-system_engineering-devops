ff89803e8aa71535f35ed0c1c04f2e8d14ecf156
Taiwo Babalola <babalolataiwop@gmail.com>
2023-05-18 12:57:18 +0100

    add fixes for nginx server

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,11 +2,6 @@
 # the web server open per request
 
 exec {'fix-nginx-server-too-many-open-files-error':
-command => 'sed -i "s/15/4096/" /etc/default/nginx',
+command => 'sed -i "s/15/4096/" /etc/default/nginx; sudo service restart nginx',
   path  => '/usr/bin:/usr/sbin:/bin',
-}
-
-exec {'restart nginx':
-  command => 'sudo service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/bin',
 }
